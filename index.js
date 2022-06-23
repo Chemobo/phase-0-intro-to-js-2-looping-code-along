0efcfa89b8a4d61bb418a321c1e686f801b1401f
James Chemobo <jkchemobo@gmail.com>
2022-06-23 11:09:53 +0300

    commit tree ad56d78b95931c3f0e99b55fe4dac08319dd40a4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const writeCards = (name) => {
+    let namesArray = [];
+    for(let i = 0; i < name.length; i++) {
+       namesArray.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return namesArray;
+ }
+ 
+ const countDown = (num) => {
+    for(let i = num; i >= 0; i--) {
+       console.log(i);
+    }
+ }
